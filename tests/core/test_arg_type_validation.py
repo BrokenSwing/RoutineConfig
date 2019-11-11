e6c7ec7eb2da57308186e51ba3f1621780226ec1
unknown <allocompte@hotmail.fr>
2019-11-11 16:11:31 +0100

    improv(test): Re-organize tests
    Making more or less tests matching tested classes'/func's packages

--- tests/core/test_arg_type_validation.py
+++ tests/core/test_arg_type_validation.py
@@ -1,52 +1,5 @@
 import unittest
-import api.arg_type
 import core.validation
-
-
-class TestArgTypes(unittest.TestCase):
-
-    def test_choice_type_multiple_choice(self):
-        arg_type = api.arg_type.choice("One", "Two", "Three")
-        self.assertDictEqual(arg_type, {
-            "type": "choice",
-            "choices": ["One", "Two", "Three"]
-        })
-
-    def test_choice_type_no_choice(self):
-        with self.assertRaises(AssertionError):
-            api.arg_type.choice()
-
-    def test_integer_type_free(self):
-        arg_type = api.arg_type.integer()
-        self.assertDictEqual(arg_type, {
-            "type": "integer"
-        })
-
-    def test_integer_type_min(self):
-        arg_type = api.arg_type.integer(minimum=10)
-        self.assertDictEqual(arg_type, {
-            "type": "integer",
-            "min": 10
-        })
-
-    def test_integer_type_max(self):
-        arg_type = api.arg_type.integer(maximum=84)
-        self.assertDictEqual(arg_type, {
-            "type": "integer",
-            "max": 84
-        })
-
-    def test_integer_type_min_lt_max(self):
-        arg_type = api.arg_type.integer(minimum=2, maximum=40)
-        self.assertDictEqual(arg_type, {
-            "type": "integer",
-            "min": 2,
-            "max": 40
-        })
-
-    def test_integer_type_min_gt_max(self):
-        with self.assertRaises(AssertionError):
-            api.arg_type.integer(minimum=50, maximum=7)
 
 
 class TestArgTypeValidation(unittest.TestCase):
@@ -171,5 +124,5 @@ class TestGlobalValidation(unittest.TestCase):
         self.assertFalse(self.calls["choice"])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
