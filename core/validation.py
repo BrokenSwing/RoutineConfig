3399fb6990cf2b56778321a50fe9adbd1247336a
unknown <allocompte@hotmail.fr>
2019-11-11 12:41:40 +0100

    feat(core): Add utility functions to validate value for a given argument type
    Add associated tests

---
+++ core/validation.py
@@ -0,0 +1,42 @@
+def validate(arg_type: dict, value: any) -> bool:
+    """
+    Validates any known argument type.
+
+    :param arg_type: argument type from api.arg_type
+    :param value: the value to validate for this argument
+    :return: True if the value if valid for the given argument type, else False
+    """
+    if arg_type["type"] == "integer" and type(value) == int:
+        return validate_integer_arg(arg_type, int(value))
+    if arg_type["type"] == "choice" and type(value) == str:
+        return validate_choice_arg(arg_type, value)
+    return False
+
+
+def validate_integer_arg(arg: dict, value: int) -> bool:
+    """
+    Checks if the given value matches constraints of integer argument (from api.arg_type).
+    If arg has min value, checks if value is equal to or greater than this min value.
+    If arg has max value, checks if value is equal to or lower than this max value.
+
+    :param arg: the integer argument
+    :param value: the value to validate
+    :return: True if the value passes checks, else False
+    """
+    if "min" in arg and value < arg["min"]:
+        return False
+    if "max" in arg and value > arg["max"]:
+        return False
+    return True
+
+
+def validate_choice_arg(arg: dict, value: str) -> bool:
+    """
+    Checks if the given value matches constraints of choice argument (from api.arg_type).
+    It checks if the given value is in the list of choices defined in argument type
+
+    :param arg: the choice argument
+    :param value: the value to validate
+    :return: True if the value passes checks, else False
+    """
+    return value in arg["choices"]
--- tests/test_arg_type.py
+++ tests/test_arg_type.py
@@ -1,5 +1,6 @@
 import unittest
 import api.arg_type
+import core.validation
 
 
 class TestArgTypes(unittest.TestCase):
@@ -46,6 +47,128 @@ class TestArgTypes(unittest.TestCase):
     def test_integer_type_min_gt_max(self):
         with self.assertRaises(AssertionError):
             api.arg_type.integer(minimum=50, maximum=7)
+
+
+class TestArgTypeValidation(unittest.TestCase):
+
+    def test_integer_validation_no_constraint(self):
+        result = core.validation.validate_integer_arg({
+            "type": "integer"
+        }, 25)
+        self.assertTrue(result)
+
+    def test_integer_validation_min_constraint(self):
+        arg = {
+            "type": "integer",
+            "min": 50
+        }
+        result = core.validation.validate_integer_arg(arg, 40)
+        self.assertFalse(result)
+
+        result = core.validation.validate_integer_arg(arg, 50)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 55)
+        self.assertTrue(result)
+
+    def test_integer_validation_max_constraint(self):
+        arg = {
+            "type": "integer",
+            "max": 22
+        }
+        result = core.validation.validate_integer_arg(arg, 20)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 22)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 64)
+        self.assertFalse(result)
+
+    def test_integer_validation_min_and_max_constraint(self):
+        arg = {
+            "type": "integer",
+            "min": 50,
+            "max": 81
+        }
+        result = core.validation.validate_integer_arg(arg, 44)
+        self.assertFalse(result)
+
+        result = core.validation.validate_integer_arg(arg, 50)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 64)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 81)
+        self.assertTrue(result)
+
+        result = core.validation.validate_integer_arg(arg, 85)
+        self.assertFalse(result)
+
+    def test_choice_validation(self):
+        arg = {
+            "type": "choice",
+            "choices": [
+                "one",
+                "more",
+                "thing"
+            ]
+        }
+
+        result = core.validation.validate_choice_arg(arg, "one")
+        self.assertTrue(result)
+
+        result = core.validation.validate_choice_arg(arg, "more")
+        self.assertTrue(result)
+
+        result = core.validation.validate_choice_arg(arg, "thing")
+        self.assertTrue(result)
+
+        result = core.validation.validate_choice_arg(arg, "two")
+        self.assertFalse(result)
+
+
+class TestGlobalValidation(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.old_i = core.validation.validate_integer_arg
+        self.old_c = core.validation.validate_choice_arg
+
+        self.calls = {
+            "integer": False,
+            "choice": False
+        }
+
+        def validate_int(arg, value):
+            self.calls["integer"] = True
+            return self.old_i(arg, value)
+
+        def validate_choice(arg, value):
+            self.calls["choice"] = True
+            return self.old_c(arg, value)
+
+        core.validation.validate_integer_arg = validate_int
+        core.validation.validate_choice_arg = validate_choice
+
+    def tearDown(self) -> None:
+        core.validation.validate_integer_arg = self.old_i
+        core.validation.validate_choice_arg = self.old_c
+
+    def test_global_validation_int(self):
+        core.validation.validate({"type": "integer"}, 5)
+        self.assertTrue(self.calls["integer"])
+        self.assertFalse(self.calls["choice"])
+
+    def test_global_validation_choice(self):
+        core.validation.validate({"type": "choice", "choices": []}, "")
+        self.assertTrue(self.calls["choice"])
+        self.assertFalse(self.calls["integer"])
+
+    def test_global_validation_unknown(self):
+        core.validation.validate({"type": "unknown"}, None)
+        self.assertFalse(self.calls["integer"])
+        self.assertFalse(self.calls["choice"])
 
 
 if __name__ == "__main__":
